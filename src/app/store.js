e7df959c02f73410db3356c3cac8a64e305f411f
Dave86dev <davidochando1986@gmail.com>
2023-01-19 16:45:10 +0100

    Redux toolkit persistence added

--- src/app/store.js
+++ src/app/store.js
@@ -3,10 +3,25 @@ import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '../pages/User/userSlice';
 import serieSlice from '../pages/serieSlice';
 
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+import thunk from 'redux-thunk';
+
+const reducers = combineReducers({
+    user: userSlice,
+    serie: serieSlice
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export default configureStore({
-    reducer: {
-        user: userSlice,
-        serie: serieSlice
-    }
-    
+    reducer: persistedReducer,
+    middleware: [thunk]
 });
--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,23 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 
 //RDX
+
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+
 import { Provider } from 'react-redux';
 import store from './app/store';
+
+let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-         <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
