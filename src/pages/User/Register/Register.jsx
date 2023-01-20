48e14f14f5ad243d878e1c1c2487092c2f34f610
Dave86dev <davidochando1986@gmail.com>
2023-01-20 11:09:43 +0100

    Cambios proyecto viernes before to leave

--- src/pages/User/Register/Register.jsx
+++ src/pages/User/Register/Register.jsx
@@ -1,10 +1,39 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { InputText } from '../../../common/InputText/InputText';
 import './Register.css';
 
 export const Register = () => {
 
+    const [usuario, setUsuario] = useState({
+        name: '',
+        password: '',
+        email: '',
+        phone: '',
+        country: ''
+    })
+
+    const registerInputHandler = (e) => {
+
+        //Manejo la entrada de datos en los input y voy actualizando el hook usuario a medida
+        //que los datos se van introduciendo
+
+        //Bindear (atar)
+        setUsuario((prevState)=>({...prevState, 
+            [e.target.name] : e.target.value
+            
+        }));;
+
+    }
+
     return (
-        <div className='registerDesign'>Soy Register</div>
+        <div className='registerDesign'>
+            <pre>{JSON.stringify(usuario, null, 2)}</pre>
+            <InputText type={'text'} name={'name'} placeholder={'nombre'} functionHandler={registerInputHandler}/>
+            <InputText type={'password'} name={'password'} placeholder={'pass'} functionHandler={registerInputHandler}/>
+            <InputText type={'text'} name={'phone'} placeholder={'telf'} functionHandler={registerInputHandler}/>
+            <InputText type={'text'} name={'country'} placeholder={'pais'} functionHandler={registerInputHandler}/>
+            <InputText type={'email'} name={'email'} placeholder={'correo'} functionHandler={registerInputHandler}/>
+        </div>
     );
 };
